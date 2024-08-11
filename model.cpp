dbe5b25cacb29b93ee4e262e6e5f84f772f04270
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-08-11 17:41:28 -0400

    model(feat): Add computation graph

--- model.cpp
+++ model.cpp
@@ -69,20 +69,30 @@ int main() {
     }
 
     // Define tensor dimensions
-    int64_t ne0 = 3; // number elements with in a row
-    int64_t ne1 = 4; // number of elements iwthin a column
+    int64_t ne0 = 3; // number of elements within a row
+    int64_t ne1 = 4; // number of elements within a column
 
     // Define the tensors to be used within the computation graph
     struct ggml_tensor* a = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, ne0, ne1);
-    ggml_set_name(a, "a"); // label the tensor for identifcation
+    ggml_set_name(a, "a"); // label the tensor for identification
 
     struct ggml_tensor* b = ggml_new_tensor_2d(ctx, GGML_TYPE_F32, ne0, ne1);
     ggml_set_name(b, "b");
 
+    // Initialize the input tensors
+    ggml_set_f32(a, 2.0f); // Initialize elements in a to 2.0f
+    ggml_set_f32(b, 1.0f); // Initialize elements in b to 1.0f
+
+    // Print initialized tensors
+    print_tensor_info(a, GGML_TYPE_F32);
+    print_tensor_info(b, GGML_TYPE_F32);
+
     // Define operations for each node within the computation graph
+    // x = a * b
     struct ggml_tensor* x = ggml_mul(ctx, a, b);
     ggml_set_name(x, "x");
 
+    // f = a * x + b
     struct ggml_tensor* f = ggml_add(ctx, ggml_mul(ctx, a, x), b);
     ggml_set_name(f, "f");
 
@@ -90,24 +100,14 @@ int main() {
     struct ggml_cgraph* gf = ggml_new_graph(ctx);
     ggml_build_forward_expand(gf, f);
 
-    // Initialize the tensors and computation graph
-    ggml_set_f32(a, 2.0f); // Initialize elements in a to 2.0f
-    print_tensor_info(a, GGML_TYPE_F32);
+    // Compute the graph
+    ggml_graph_compute_with_ctx(ctx, gf, 8); // Using 8 threads
 
-    ggml_set_f32(b, 1.0f); // Initialize elements in b to 1.0f
-    print_tensor_info(b, GGML_TYPE_F32);
+    // Print the output tensor after computation
+    print_tensor_info(x, GGML_TYPE_F32); // --> 2.0f
+    print_tensor_info(f, GGML_TYPE_F32); // --> 5.0f
 
-    ggml_set_f32(x, 3.0f); // Initialize elements in x to 3.0f
-    print_tensor_info(x, GGML_TYPE_F32);
-    // same as ggml_graph_compute() but the work data is allocated as a part of
-    // the context note: the drawback of this API is that you must have ensured
-    // that the context has enough memory for the work data
-    // ggml_status ggml_graph_compute_with_ctx(
-    //     ggml_context *ctx, ggml_cgraph *cgraph, int n_threads
-    // )
-    ggml_graph_compute_with_ctx(ctx, gf, 8);
-    print_tensor_info(f, GGML_TYPE_F32);
-
+    // Clean up
     ggml_free(ctx);
 
     return 0;
