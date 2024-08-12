cb4449fd8593451e71a7058692fc0fde6937871e
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-08-12 14:16:09 -0400

    xor(model): Add initial sketch for outlining the xor model

--- xor.cpp
+++ xor.cpp
@@ -19,3 +19,33 @@
  * Note: This file is currently a stub and will be developed further as
  * part of the ongoing exploration and application of the GGML library.
  */
+
+#include "ggml.h"
+
+struct xor_hparams {
+    uint32_t n_input  = 2; // XOR has 2 input features
+    uint32_t n_hidden = 4; // Number of hidden units (adjustable)
+    uint32_t n_output = 1; // XOR has 1 output
+};
+
+struct xor_layer {
+    struct ggml_tensor* weights;
+    struct ggml_tensor* biases;
+};
+
+struct xor_model {
+    struct ggml_context* ctx = NULL;
+    struct xor_hparams   hparams;
+    struct xor_layer     input_layer; // Weights and biases for input to hidden
+    struct xor_layer hidden_layer;    // Weights and biases for hidden to output
+};
+
+int main(void) {
+    // implementation steps:
+    //   1. initialization
+    //   2. forward pass
+    //   3. backward pass
+    //   4. error handling
+
+    return 0;
+}
