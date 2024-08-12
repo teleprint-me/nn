4196616dc8f281d34955c87f6df052bd7291cb58
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-08-12 15:42:04 -0400

    xor(feat): Add initialization block for xor models

--- xor.cpp
+++ xor.cpp
@@ -40,9 +40,44 @@ struct xor_model {
     struct xor_layer hidden_layer;    // Weights and biases for hidden to output
 };
 
+xor_model init_xor_model(void) {
+    // we allocate to the stack for simplicity, though we could manually
+    // allocate memory if we chose to. GGML does not have anything builtin to
+    // handle this allocation, so we would be responsible for its management,
+    // hence why we opt for the stack instead
+    xor_model model;
+
+    ggml_init_params params
+        = {.mem_size = 16 * 1024 * 1024, .mem_buffer = NULL, .no_alloc = false};
+
+    model.ctx = ggml_init(params);
+
+    // initialize the input layers
+    // the models neurons will have 2 inputs and 1 ouput
+    model.input_layer.weights = ggml_new_tensor_2d(
+        model.ctx, GGML_TYPE_F32, model.hparams.n_hidden, model.hparams.n_input
+    );
+    model.input_layer.biases = ggml_new_tensor_2d(
+        model.ctx, GGML_TYPE_F32, model.hparams.n_hidden, 1
+    );
+
+    // initialize the hidden layers
+    // route the outputs to the hidden neurons
+    model.hidden_layer.weights = ggml_new_tensor_2d(
+        model.ctx, GGML_TYPE_F32, model.hparams.n_output, model.hparams.n_hidden
+    );
+    model.hidden_layer.biases = ggml_new_tensor_2d(
+        model.ctx, GGML_TYPE_F32, model.hparams.n_output, 1
+    );
+
+    return model;
+}
+
 int main(void) {
     // implementation steps:
     //   1. initialization
+    xor_model model = init_xor_model();
+
     //   2. forward pass
     //   3. backward pass
     //   4. error handling
